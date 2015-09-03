1ddbe69834b5fa2b4ae3f4e83454db220096f90b
DiscanX <discan123@gmail.com>
2015-09-02 20:15:44 -0400

    Changing bishop same pawn color penality to include the enemy's

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -178,7 +178,7 @@ namespace {
   const Score RookOnPawn         = S( 7, 27);
   const Score RookOnOpenFile     = S(43, 21);
   const Score RookOnSemiOpenFile = S(19, 10);
-  const Score BishopPawns        = S( 8, 12);
+  const Score BishopPawns        = S( 4,  6);
   const Score MinorBehindPawn    = S(16,  0);
   const Score TrappedRook        = S(92,  0);
   const Score Unstoppable        = S( 0, 20);
@@ -304,9 +304,10 @@ namespace {
                 && (pos.pieces(PAWN) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
 
-            // Penalty for pawns on same color square of bishop
+            // Penalty for all pawns on same color square of bishop
             if (Pt == BISHOP)
-                score -= BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s);
+                score -= BishopPawns * (ei.pi->pawns_on_same_color_squares(Us, s)
+			 	      + ei.pi->pawns_on_same_color_squares(Them, s));
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
