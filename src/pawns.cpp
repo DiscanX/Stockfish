943ff0f0244454fd411d09818e72c0d4245f4093
keven <discan123@gmail.com>
2017-04-23 20:27:30 -0400

    Remove A and F files from bishop pawns evaluation (with fix)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -108,13 +108,14 @@ namespace {
 
     Bitboard ourPawns   = pos.pieces(Us  , PAWN);
     Bitboard theirPawns = pos.pieces(Them, PAWN);
-    Bitboard AHFiles    = FileABB | FileHBB;
+    Bitboard filesWithoutEdges = ~(FileABB | FileHBB);
+
     e->passedPawns[Us]   = e->pawnAttacksSpan[Us] = 0;
     e->semiopenFiles[Us] = 0xFF;
     e->kingSquares[Us]   = SQ_NONE;
     e->pawnAttacks[Us]   = shift<Right>(ourPawns) | shift<Left>(ourPawns);
-    e->pawnsOnSquaresExceptSides[Us][BLACK] = popcount((ourPawns & DarkSquares) ^ AHFiles);
-    e->pawnsOnSquaresExceptSides[Us][WHITE] = popcount((ourPawns & LightSquares) ^ AHFiles);
+    e->pawnsOnSquaresExceptSides[Us][BLACK] = popcount((ourPawns & DarkSquares & filesWithoutEdges));
+    e->pawnsOnSquaresExceptSides[Us][WHITE] = popcount((ourPawns & LightSquares & filesWithoutEdges));
 
     // Loop through all pawns of the current color and score each pawn
     while ((s = *pl++) != SQ_NONE)
